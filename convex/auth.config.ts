8696c27d2493d85a50459a8efd7df7df8edd2dd1
Pratik <prateekraiger123@gmail.com>
2025-04-12 15:44:31 +0530

    update convex client

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://unique-mammoth-32.clerk.accounts.dev",
+      domain: "https://complete-ant-51.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
